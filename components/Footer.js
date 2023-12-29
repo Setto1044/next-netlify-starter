15cefce766289d23ae82f3ff6ff3893e0af023dc
Setto1044 <baesj1044@gmail.com>
2023-12-29 16:00:11 +0900

    rename

--- components/Footer.js
+++ components/Footer.js
@@ -4,9 +4,9 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        /*
-        //<img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
-        */
+        
+        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+        
       </footer>
     </>
   )
--- pages/index.js
+++ pages/index.js
@@ -9,14 +9,14 @@ export default function Home() {
     <div className="container">
       {/* Head section for metadata */}
       <Head>
-        <title>Hi Nabong</title>
+        <title>Test </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       {/* Main section */}
       <main>
         {/* Header component with a title */}
-        <Header title="나희양, 이상한거 그만보세요." />
+        <Header title="테스트 사이트" />
 
         {/* Body section */}
         <body>
@@ -25,8 +25,8 @@ export default function Home() {
 
           {/* Additional text and link */}
           <p>
-            주무실 시간이에요. 
-            <a href="https://www.google.com/search?q=sleepy+cat&sca_esv=594242284&hl=ko&tbm=isch&source=lnms&sa=X&ved=2ahUKEwiIlPXEs7KDAxWMdN4KHcl1BOIQ_AUoAXoECAMQAw&biw=1536&bih=703&dpr=1.25">고양이를 보려면 여기를 클릭</a>.
+            링크 테스트 
+            <a href="https://www.google.com/">구글로 가려면 여기를 클릭</a>.
           </p>
         </body>
       </main>
